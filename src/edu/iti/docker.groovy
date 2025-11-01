6d0285df6b13ff134567c54eed9583dcaf6eab06
ayman <ayman.mohamed141163@el-eng.menofia.edu.eg>
2025-11-01 21:33:37 +0200

    edit deployment

--- src/edu/iti/docker.groovy
+++ src/edu/iti/docker.groovy
@@ -13,3 +13,16 @@ def deploy(imageName,  imgTag) {
     sh "docker run -d --name ${imageName}-container -p 8080:8080 ${imageName}:${imgTag}"
     }
 
+def deploy(String imageName, String imgTag) {
+    script.echo "Deploying container ${imageName}:${imgTag}..."
+
+    def safeName = imageName.replaceAll('/', '-')
+
+    script.sh """
+        docker stop ${safeName}-container || true
+        docker rm ${safeName}-container || true
+        docker run -d --name ${safeName}-container -p 8080:8080 ${imageName}:${tag}
+    """
+
+    script.echo "Deployment completed!"
+}
